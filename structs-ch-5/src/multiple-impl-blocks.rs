5917203555da0bf5448d47096632f156e5723d97
freddyal <freddyal0x01@gmail.com>
2022-04-14 22:57:17 -0400

    same as prior commit

---
+++ structs-ch-5/src/multiple-impl-blocks.rs
@@ -0,0 +1,16 @@
+#[derive(Debug)]
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+// Each struct is allowed to have multiple impl blocks. 
+
