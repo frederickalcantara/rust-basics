0191fb60806f1262746cde7c606cd9f373a7bf96
freddyal <freddyal0x01@gmail.com>
2022-04-14 23:00:35 -0400

    Added base code for ch 5 struct stuff

--- structs-ch-5/src/main.rs
+++ structs-ch-5/src/main.rs
@@ -1,3 +1,26 @@
 // Structs let you create custom types that are meaningful for your domain. 
 // By using structs, you can keep associated pieces of data connected to each other and name each piece to make your code clear. 
 // In impl blocks, you can define functions that are associated with your type, and methods are a kind of associated function that let you specify the behavior that instances of your structs have.
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+}
