113a963d42fea48f36044dd49390ffd5bae25fe5
freddyal <freddyal0x01@gmail.com>
2022-04-14 22:56:42 -0400

    Added code to explain multiple blocks

--- structs-ch-5/src/main.rs
+++ structs-ch-5/src/main.rs
@@ -1,26 +1,16 @@
 #[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
 
 impl Rectangle {
-    fn square(size: u32) -> Rectangle {
-        Rectangle {
-            width: size,
-            height: size
-        }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
-    // All functions defined in the impl block are called association functions, 
-    // because they're associated with the type named after the impl. 
-    
-    // We can define associated functions that don’t have self as their first parameter (and thus are not methods) 
-    // because they don’t need an instance of the type to work with.
-
-    // Associated functions that aren’t methods are often used for constructors that will return a new instance of the struct.
 }
-fn main() {
-    sq_size = Rectangle::square(3);
 
-    println!("Square size: {}", sq_size);
-}
+// Each struct is allowed to have multiple impl blocks. 
+
