c78b70633726cf1b0efc7a97bec6b9936799a149
Frederick Alcantara <falcantara@linode.com>
2022-04-21 10:19:05 -0400

    Added example code for hashmaps

--- collections-ch-8/src/main.rs
+++ collections-ch-8/src/main.rs
@@ -1,3 +1,18 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+
+let text = "hello world wonderful world";
+
+let mut map = HashMap::new();
+
+for word in text.split_whitespace() {
+    println!("{}", word);
+    let count = map.entry(word).or_insert(0);
+    println!("Before Count: {}", count);
+    *count += 1;
+    println!("After Count: {}", count);
+}
+
+println!("{:?}", map);
 }
